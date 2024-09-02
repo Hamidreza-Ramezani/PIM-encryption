3a697c4489b9875aa307879db809294143503974
hamidreza-ramezani <hamid.ramezani1375@gmail.com>
2024-09-02 13:57:42 -0700

    minor

--- script.sh
+++ script.sh
@@ -1,44 +1,30 @@
 #!/usr/sh 
 
 data_file="data/host.csv"
-if [ -f "$data_file" ]; then
-    rm "$data_file"
-fi
-
-echo "Operation,Data size, execution time" > $data_file
 make experiment  > /dev/null
+echo "Operation,Data size, execution time\n" > $data_file
+data_size=16
 
-#for mode in host aesni; do
-for mode in host; do
-data_file="data/host.csv"
-for operation in encrypt
+while [ $data_size -le $((1 << 35)) ]
 do
-	data_size=16384
-	while [ $data_size -le $((1 << 35)) ]
-	do
-		for i in {1..1}
-		do
-			echo "Testing with $data_size bytes in $mode mode..."
-			./experiment/pimcrypto $mode $operation $data_size >> $data_file
-		done
-		data_size=$(($data_size * 2))
-	done
-done
+	echo "Testing with $data_size bytes on the host..."
+	./experiment/pimcrypto host encrypt $data_size >> $data_file
+	data_size=$(($data_size * 2))
 done
 
 
+awk 'BEGIN{FS=","; OFS=","} NR==1{print "version", "parallelism", "time"} NR>1 {print $1, $2, $3}' "data/host.csv" > "data/host-graph.csv"
+
+rm "data/host.csv"
 
 data_file="data/cost.csv"
-if [ -f "$data_file" ]; then
-    rm "$data_file"
-fi
-make experiment  > /dev/null
 
 echo "Tasklets,DPUs,Operation,Data size,Allocation time,Loading time,Data" \
 " copy in,Parameter copy in,Launch,Data copy out,Performance count copy" \
 " out,Free DPUs,Performance count min, max, average" > $data_file
+make experiment  > /dev/null
 
-for ranks in {1..9}; do
+for ranks in {1..36}; do
 	data_per_dpu=16
 	while [ $data_per_dpu -lt $((2 << 26)) ]
 	do
@@ -49,9 +35,10 @@ for ranks in {1..9}; do
 	done
 done
 
-awk 'BEGIN{FS=","; OFS=","} NR==1{print "version", "parallelism", "time"} NR>1 {print "dpus="$2, $4/2^20, $5+$6+$7+$8+$9+$10+$11+$12}' $data_file > "data/dpu_all_included.csv"
 
-awk 'BEGIN{FS=","; OFS=","} NR==1{print "version", "parallelism", "time"} NR>1 {print "dpus="$2, $4/2^20, $9}' $data_file > "data/dpu_only_launch.csv"
+awk 'BEGIN{FS=","; OFS=","} NR==1{print "version", "parallelism", "time"} NR>1 {print "dpus="$2, $4, $5+$6+$7+$8+$9+$10+$11+$12}' $data_file > "data/dpu_all_included.csv"
+
+awk 'BEGIN{FS=","; OFS=","} NR==1{print "version", "parallelism", "time"} NR>1 {print "dpus="$2, $4, $9}' $data_file > "data/dpu_only_launch.csv"
 
 
 
